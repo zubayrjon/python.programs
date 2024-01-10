f65ef8fa8e9719af9a7dbe7fcd8ebb76d95f9043
zubayrjon <156114387+zubayrjon@users.noreply.github.com>
2024-01-10 06:48:40 +0500

    ikkinchi fayl yuklandi

---
+++ dars2.py
@@ -0,0 +1,7 @@
+a = int(input('Yoshingizni kiriting: \n____'))
+if a<18:
+    print("Siz maktab o'quvchisisiz")
+elif a<7:
+    print("Siz maktab o'quvchisi emassiz")
+else:
+    print("Siz maktab o'quvchisi emassiz")
